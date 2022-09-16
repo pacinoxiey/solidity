134b770083b32ccd6dfe8ee4387f2cee5c05a922
xiey <1004056738@qq.com>
2022-09-16 22:37:03 +0800

    实现IERC721全部接口

--- ERC721/ERC721.sol
+++ ERC721/ERC721.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8.7;
 
 import "./IERC721.sol";
 import "./IERC721Metadata.sol";
+import "./IERC721Receiver.sol";
 
 //引入library库
 import "../utils/Strings.sol";
+import "../utils/Address.sol";
 
 contract ERC721 is IERC721Metadata, IERC721{
 
     using Strings for uint256; // 使用String库，
-
+    using Address for address; // 使用Address库, 添加了判断是否是合约方法
 
     string public override name;
     string public override symbol;
@@ -55,7 +57,7 @@ contract ERC721 is IERC721Metadata, IERC721{
     }
 
     //返回持有人
-    function ownerOf(uint256 tokenId) external view override returns (address owner){
+    function ownerOf(uint256 tokenId) public view override returns (address owner){
         return _owners[tokenId];
     }
 
@@ -67,24 +69,132 @@ contract ERC721 is IERC721Metadata, IERC721{
         bytes calldata data
     ) external override {
         address owner = _owners[tokenId];
-        require(_isApproveOrOwner(owner, msg.sender, tokenId), "not owner nor approved")
-        
+        require(_isApprovedOrOwner(owner, msg.sender, tokenId), "not owner nor approved");
+        _safeTransfer(owner, from, to, tokenId, data);
     }
 
+    //不带data的转账
     function safeTransferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) external;
+    ) external override {
+        address owner = _owners[tokenId];
+        require(_isApprovedOrOwner(owner, msg.sender, tokenId), "not owner nor approved");
+        _safeTransfer(owner, from, to, tokenId, "");
+    }
 
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external override {
+        address owner = ownerOf(tokenId);
+        require(
+            _isApprovedOrOwner(owner, msg.sender, tokenId),
+            "not owner nor approved"
+        );
+        _transfer(owner, from, to, tokenId);
+    }
+
+    //授权, tokenId必须属于发起者, 或者被授权
+    function approve(address to, uint256 tokenId) external override {
+        address owner = _owners[tokenId];
+        require(
+            msg.sender == owner || _operatorApprovals[owner][msg.sender],
+            "not owner nor approved for all"
+        );
+        _approve(owner, to, tokenId);
+    }
+
+    //添加批量授权得映射, 释放事件
+    function setApprovalForAll(address operator, bool _approved) external override {
+        _operatorApprovals[msg.sender][operator] = _approved;
+        emit ApprovalForAll(msg.sender, operator, _approved);
+    }
+
+    function getApproved(uint256 tokenId) external view override returns (address operator) {
+        require(_owners[tokenId] != address(0), "token doesn't exist");
+        return _tokenApprovals[tokenId];
+    }
+
+    //获取是否全部授权
+    function isApprovedForAll(address owner, address operator) external view override returns (bool){
+        return _operatorApprovals[owner][operator];
+    }
+
+    //私有方法-------------------------------------------------------------------------------
     //判断操作者是否是拥有者或者被授权
-    function _isApproveOrOwner(address owner, address spender, uint tokenId)
+    function _isApprovedOrOwner(address owner, address spender, uint tokenId)
     private view returns(bool) {
         return (spender == owner || _tokenApprovals[tokenId] == spender
-        || _operatorApprovals[owner][spender])
+        || _operatorApprovals[owner][spender]);
     } 
 
-    function _safeTransfer(address from, address to, uint tokenId) private view  {
+    /**
+     * 安全转账，安全地将 tokenId 代币从 from 转移到 to，会检查合约接收者是否了解 ERC721 协议，以防止代币被永久锁定。调用了_transfer函数和_checkOnERC721Received函数。条件：
+     * from 不能是0地址.
+     * to 不能是0地址.
+     * tokenId 代币必须存在，并且被 from拥有.
+     * 如果 to 是智能合约, 他必须支持 IERC721Receiver-onERC721Received.
+     */
+    function _safeTransfer(
+        address owner,
+        address from,
+        address to,
+        uint tokenId,
+        bytes memory _data
+    ) private {
+        _transfer(owner, from, to, tokenId);
+        require(_checkOnERC721Received(from, to, tokenId, _data), "not ERC721Receiver");
+    }
 
+    /*
+     * 转账函数。通过调整_balances和_owner变量将 tokenId 从 from 转账给 to，同时释放Tranfer事件。
+     * 条件:
+     * 1. tokenId 被 from 拥有
+     * 2. to 不是0地址
+     */
+    function _transfer(
+        address owner,
+        address from,
+        address to,
+        uint tokenId
+    ) private {
+        require(from == owner, "not owner");
+        require(to != address(0), "transfer to the zero address");
+
+        _approve(owner, address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function _approve(address owner, address to, uint tokenId) private{
+        _tokenApprovals[tokenId] = to;
+        emit Approval(owner, to, tokenId);
+    }
+
+    // _checkOnERC721Received：函数，用于在 to 为合约的时候调用IERC721Receiver-onERC721Received, 以防 tokenId 被不小心转入黑洞。
+    function _checkOnERC721Received(
+        address from,
+        address to,
+        uint tokenId,
+        bytes memory _data
+    ) private returns (bool) {
+        if (to.isContract()) {
+            return
+                IERC721Receiver(to).onERC721Received(
+                    msg.sender,
+                    from,
+                    tokenId,
+                    _data
+                ) == IERC721Receiver.onERC721Received.selector;
+        } else {
+            return true;
+        }
     }
 }
