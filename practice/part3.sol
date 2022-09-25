b03eca85c798beb890dd1a2210136e2b946ede7d
xiey <1004056738@qq.com>
2022-09-25 12:37:15 +0800

    fallback和receive的执行逻辑

---
+++ practice/part3.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity ^0.8.7;
+
+/* 触发fallback() 还是 receive()?
+           接收ETH
+              |
+         msg.data是空？
+            /  \
+          是    否
+          /      \
+receive()存在?   fallback()
+        / \
+       是  否
+      /     \
+receive()  fallback   
+    */
+
+contract part3{
+
+    event receiveEvent(address indexed from, uint256 value);
+    event fallbackEvent(address indexed from, uint256 value, bytes data);
+
+    receive() external payable {
+        emit receiveEvent(msg.sender, msg.value);
+    }
+
+    fallback() external payable {
+        emit fallbackEvent(msg.sender, msg.value, msg.data);
+    }
+
+    function test() external view returns (address a, address b){
+        a = address(this);
+        b = msg.sender;
+    }
+
+    function test2() external view returns (uint){
+        return msg.sender.balance;
+    }
+}
